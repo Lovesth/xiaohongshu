3b4aad48f42f76b50a98ca54fc83754425fed086
xmh <1667286082@qq.com>
2025-05-19 20:48:06 +0800

    commit

--- xiaohashu/xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/TestController.java
+++ xiaohashu/xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.quanxiaoha.xiaohashu.auth.controller;
 
+import com.quanxiaoha.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.quanxiaoha.framework.common.response.Response;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/test")
+    @ApiOperationLog(description = "测试接口")
     public Response<String> test() {
         return Response.success("Hello, 犬小哈专栏");
     }
